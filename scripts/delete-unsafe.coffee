d797735eb9949281b2e3a1e7f6ba1cffa85c04ea
Igor Ramadas <igor@devv.com>
2018-06-26 14:59:32 +0200

    Script to delete unsafe files almost ready.

--- scripts/delete-unsafe.coffee
+++ scripts/delete-unsafe.coffee
@@ -3,8 +3,44 @@
 fs = require "fs"
 path = require "path"
 
-try
-    folders = Array::slice.call process.argv, 2
+console.log ""
+console.log "#######################################################"
+console.log "###         - IMGRecog.js - delete unsafe -         ###"
+console.log "#######################################################"
+console.log ""
 
-    for f in folders
-        contents = fs.readdirSync f
+# Minimum score to consider.
+score = 0.9
+
+# Array of files to be deleted.
+toDelete = []
+
+try
+    params = Array::slice.call process.argv, 2
+
+    for folder in params
+        console.log ""
+        console.log "Folder: #{folder}"
+
+        files = fs.readdirSync folder
+
+        for file in files
+            if path.extname(file) is ".tags"
+                try
+                    filepath = path.join(folder, file)
+                    tags = fs.readFileSync filepath, "UTF-8"
+                    tags = JSON.parse tags
+
+                    toDelete.push filepath if tags.adult? and tags.adult > score
+                    toDelete.push filepath if tags.violence? and tags.violence > score
+                catch ex
+                    console.error filepath, ex
+
+    # Delete unsafe files.
+    for file in toDelete
+        do (file) ->
+            fs.unlink file, (err) ->
+                if err?
+                    console.error file, err
+                else
+                    console.log file, "deleted!"
--- scripts/delete-unsafe.js
+++ scripts/delete-unsafe.js
@@ -1,16 +1,66 @@
 (function() {
   // This script will delete all images that have
-  var contents, f, folders, fs, i, len, path;
+  var ex, file, filepath, files, folder, fs, i, j, k, len, len1, len2, params, path, score, tags, toDelete;
 
   fs = require("fs");
 
   path = require("path");
 
+  console.log("");
+
+  console.log("#######################################################");
+
+  console.log("###         - IMGRecog.js - delete unsafe -         ###");
+
+  console.log("#######################################################");
+
+  console.log("");
+
+  // Minimum score to consider.
+  score = 0.9;
+
+  // Array of files to be deleted.
+  toDelete = [];
+
   try {
-    folders = Array.prototype.slice.call(process.argv, 2);
-    for (i = 0, len = folders.length; i < len; i++) {
-      f = folders[i];
-      contents = fs.readdirSync(f);
+    params = Array.prototype.slice.call(process.argv, 2);
+    for (i = 0, len = params.length; i < len; i++) {
+      folder = params[i];
+      console.log("");
+      console.log(`Folder: ${folder}`);
+      files = fs.readdirSync(folder);
+      for (j = 0, len1 = files.length; j < len1; j++) {
+        file = files[j];
+        if (path.extname(file) === ".tags") {
+          try {
+            filepath = path.join(folder, file);
+            tags = fs.readFileSync(filepath, "UTF-8");
+            tags = JSON.parse(tags);
+            if ((tags.adult != null) && tags.adult > score) {
+              toDelete.push(filepath);
+            }
+            if ((tags.violence != null) && tags.violence > score) {
+              toDelete.push(filepath);
+            }
+          } catch (error) {
+            ex = error;
+            console.error(filepath, ex);
+          }
+        }
+      }
+    }
+// Delete unsafe files.
+    for (k = 0, len2 = toDelete.length; k < len2; k++) {
+      file = toDelete[k];
+      (function(file) {
+        return fs.unlink(file, function(err) {
+          if (err != null) {
+            return console.error(file, err);
+          } else {
+            return console.log(file, "deleted!");
+          }
+        });
+      })(file);
     }
   } catch (error) {}
 
